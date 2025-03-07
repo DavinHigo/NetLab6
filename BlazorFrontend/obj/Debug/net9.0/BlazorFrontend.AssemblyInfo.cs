07ea0b295f188bb7ac160597b5d6d99c938eb97e
Davin Higo <dhigo1@my.bcit.ca>
2025-03-06 16:22:44 -0800

    commit 3

--- BlazorFrontend/obj/Debug/net9.0/BlazorFrontend.AssemblyInfo.cs
+++ BlazorFrontend/obj/Debug/net9.0/BlazorFrontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b6a2a9939d5e0027950d4b5a8cc249c9a97cc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
